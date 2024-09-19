c16a86b00e3f638e84a946a5f814a75aad2c06c0
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-19 23:45:38 +0530

    Assignment Complete

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let timer = document.getElementById("timer");
+
+function clock() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let day = date.getDay()
+    let mon = date.getMonth();
+    let year = date.getFullYear()
+    // console.log(h, m, s);
+    // console.log(ampm);
+    if (h==0) {
+        timer.innerHTML = `${mon}/${day}/${year}, ${h+12}:${m}:${s} AM`;   
+    }
+    if (h > 0 && h < 12) {
+        timer.innerHTML = `${mon}/${day}/${year}, ${h}:${m}:${s} AM`;   
+    }
+    if (h == 12) {
+        timer.innerHTML = `${mon}/${day}/${year}, ${h}:${m}:${s} PM`;   
+    }
+    if (h>12 && h < 24) {
+        timer.innerHTML = `${mon}/${day}/${year}, ${h-12}:${m}:${s} PM`;  
+    }
+
+    setTimeout(clock, 1000)
+}
+clock();
+
